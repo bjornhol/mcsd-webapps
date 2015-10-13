82163801b1dde7c0e9e137c0ccf536b0425a58af
Bjørn Harald Vognild-Olsen <bjornhol@gmail.com>
2015-10-13 09:45:12 +0200

    asdf

--- ProgrammingCSharp/MultiThreading.cs
+++ ProgrammingCSharp/MultiThreading.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace ProgrammingCSharp
 {
+    /// <summary>
+    /// Implement multithreading and asynchronous processing
+    ///     Use the Task Parallel library(ParallelFor, Plinq, Tasks); create continuation tasks; spawn threads by using ThreadPool; 
+    ///     unblock the UI; use async and await keywords; manage data by using concurrent collections
+    /// 
+    /// Manage multithreading
+    ///     Synchronize resources; implement locking; cancel a long-running task; implement thread-safe methods to handle race conditions
+    /// </summary>
     public class MultiThreading
     {
         public void Foo()
